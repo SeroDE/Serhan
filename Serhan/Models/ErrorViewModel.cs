84a4aadb4f6d68e56d7e91d8a8e02e77965955fc
serha <serha@DESKTOP-RB3S70M>
2023-12-01 00:39:48 +0300

    Add project files.

---
+++ Serhan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Serhan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
